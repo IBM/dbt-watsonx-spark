9eb729c81690ebfcdaad43a1867a92413495907d
Manjot Sidhu <ManjotSidhu@ibm.com>
2024-09-02 18:23:26 +0530

    chore: bump to 0.0.2

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def _get_plugin_version_dict():
 
 
 package_name = "dbt-watsonx-spark"
-package_version = "0.0.1"
+package_version = "0.0.2"
 description = """IBM watsonx.data spark plugin for dbt"""
 
 setup(
