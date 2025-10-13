13ad2516646ab3641c5be256bc6c6d5ad41cff76
Reema Alzaid <reema-alzaid@outlook.com>
2025-10-13 18:00:47 +0300

    update: update version for new release

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def _get_plugin_version_dict():
 
 
 package_name = "dbt-watsonx-spark"
-package_version = "0.1.0"
+package_version = "0.1.1"
 description = """IBM watsonx.data spark plugin for dbt"""
 
 setup(
